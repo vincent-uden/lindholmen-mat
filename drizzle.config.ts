fce1abf3d221e99dfbbb604d09dd3985dd4f2273
Vincent Udén <vincentuden@gmail.com>
2025-03-25 13:48:32 +0100

    Initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+	schema: "./src/server/db/schema.ts",
+	dialect: "turso",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+		authToken: env.DATABASE_TOKEN,
+	},
+	tablesFilter: ["lindholmen-mat_*"],
+} satisfies Config;
