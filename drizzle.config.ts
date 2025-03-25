8646a884da557cece7ac23863b309a43ecf0e557
Vincent Udén <vincentuden@gmail.com>
2025-03-25 14:05:07 +0100

    Formatting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,11 @@ import type { Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-	schema: "./src/server/db/schema.ts",
-	dialect: "turso",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-		authToken: env.DATABASE_TOKEN,
-	},
-	tablesFilter: ["lindholmen-mat_*"],
+  schema: "./src/server/db/schema.ts",
+  dialect: "turso",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_TOKEN,
+  },
+  tablesFilter: ["lindholmen-mat_*"],
 } satisfies Config;
