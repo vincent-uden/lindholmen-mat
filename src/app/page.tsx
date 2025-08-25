845486b08aae5f71f0191a69a1034af7b1c94ad8
Vincent Udén <vincentuden@gmail.com>
2025-08-25 17:25:22 +0200

    Isr test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { LatestPost } from "~/app/_components/post";
 import { HydrateClient, api } from "~/trpc/server";
 import Landing from "./_components/landing";
 
+export const revalidate = 3600;
+
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
